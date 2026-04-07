dcec8e1b370ce0e53e7f0e8ef50dc4a88ecd5c08
chandra parmar <chandra.parmar2004@gmail.com>
2026-04-07 13:29:23 +0530

    logout feature added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,43 +1,73 @@
-import React from 'react'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { Link,  } from 'react-router-dom'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { removeUser } from '../utils/userSlice'
+
 
 const Navbar = () => {
+
+   //
+   const user = useSelector((store) => store.user )
+   const dispatch = useDispatch()
+   const navigate = useNavigate()
+
+   //handle logout api 
+   const handleLogout = async()=>{
+    try{
+
+      const res = await axios.post('http://localhost:5001/api/logout',{},
+        {
+          withCredentials:true
+        })
+
+        //clear data from redux store
+        dispatch(removeUser())
+
+       return navigate('/login')
+
+    }catch(err)
+    {
+       console.log(err)
+    }
+   }
+
   return (
-    <div>
-        
-         <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-100 shadow-sm px-4">
+      <div className="flex-1">
 
-            <div className="flex-1">
-             <a className="btn btn-ghost text-xl">daisyUI</a>
+        <Link to='/' className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+
+      </div>
+
+      {user && (
+        <div className="flex items-center gap-3">
+          <span className="hidden sm:inline text-sm">Welcome, {user.firstName}</span>
+
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full overflow-hidden">
+                <img alt="user photo" src={user.photoUrl} />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 w-52 bg-base-100 rounded-box p-2 shadow z-50">
+              <li>
+
+                <Link to='/profile' className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </Link>
+
+              </li>
+              <li><a>Settings</a></li>
+              <li><a onClick={handleLogout}>Logout</a></li>
+            </ul>
           </div>
-
-          <div className="flex gap-2">
-            
-
-            <div className="dropdown dropdown-end mr-10">
-              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-
-                <div className="w-10 rounded-full">
-
-                  <img
-                    alt="Tailwind CSS Navbar component"
-                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                </div>
-           </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-          </div>
+        </div>
+      )}
     </div>
   )
 }
