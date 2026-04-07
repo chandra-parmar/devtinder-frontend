9d0979c9d8ef1db2fe29f9e3ebda00b9242433c0
chandra parmar <chandra.parmar2004@gmail.com>
2026-04-07 13:32:32 +0530

    route added for body feed login profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,38 @@
 
 import './App.css'
 import Body from './Body'
+import Login from './components/Login'
+
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore';
+import Feed from './components/Feed';
+import Profile from './components/Profile';
+
 
 function App() {
   
 
   return (
 
-      <div>
-        
-        <BrowserRouter basename='/'>
+      <div> 
+
+      <Provider store={ appStore }>
+          <BrowserRouter basename='/'>
+             
            <Routes>
+              
             <Route path='/' element={<Body></Body>}></Route>
+            <Route path='/feed' element={<Feed></Feed>}></Route>
+            <Route path='/login' element={ <Login></Login>}></Route>
+            <Route path='/profile' element={<Profile></Profile>}></Route>
+            
            </Routes>
         </BrowserRouter>
+      </Provider>
+        
+       
   
           
       </div>
