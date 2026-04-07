a1a8acc908d68c3e2ffa4b2af97d9ab066817db5
chandra parmar <chandra.parmar2004@gmail.com>
2026-04-07 13:31:30 +0530

    view profile api added

--- client/src/Body.jsx
+++ client/src/Body.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
+import {  Outlet, useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { addUser }  from './utils/userSlice'
+import axios from 'axios'
+import Profile from './components/Profile'
+import {useSelector } from 'react-redux'
 
-const Body = () => {
+function Body() {
+
+      //hooks
+      const dispatch = useDispatch()
+      const navigate = useNavigate()
+      const userData = useSelector((store)=> store.user)
+
+
+     //api call
+      const fetchUser = async() =>{
+
+        //if userpresent in store logged in then dont call again api for fetch 
+          if(userData)
+          {
+            return 
+          }
+        try{
+
+          const res = await axios.get("http://localhost:5001/api/profile/view",{
+            withCredentials: true
+          })
+
+          //update store
+          dispatch(addUser(res.data))
+
+        }catch(err)
+        {
+          //if user not logged in  
+           if(err.status === 401)
+           {
+             navigate('/login')
+           }
+            
+            console.log(err)
+        }
+
+  }
+
+
+  useEffect(()=>{
+    //if user present in store then dont call api 
+    fetchUser()
+   
+  },[])
+
+
   return (
     <div>
-        <Navbar></Navbar>
+       
+       <Navbar></Navbar>
         <Outlet></Outlet>
+        <Profile></Profile>
     </div>
   )
 }
