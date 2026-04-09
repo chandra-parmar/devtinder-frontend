debc967194c67fafda41e0e09f656d58458a009e
chandra parmar <chandra.parmar2004@gmail.com>
2026-04-09 08:58:38 +0530

    feed page added

---
+++ client/src/components/Feed.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addFeed } from '../utils/feedSlice'
+import { useEffect } from 'react'
+import axios from 'axios'
+import UserCard from '../components/UserCard'
+
+function Feed() {
+
+   const dispatch = useDispatch()
+   const feed = useSelector((store) => store.feed)
+
+  //get feed
+  const getFeed = async()=>{
+
+    try{
+      
+      //if feed already present then return 
+      if(feed)
+      {
+        return 
+      }
+
+       const res = await axios.get('http://localhost:5001/api/feed',{
+         withCredentials:true
+       })
+
+       //add feed data to store
+       console.log('feed response:', res?.data)
+       dispatch(addFeed(res?.data?.data || []))
+
+       
+
+    }catch(err)
+    {
+      console.log(err)
+    }
+  }
+
+  useEffect(()=>{
+    getFeed()
+  },[])
+
+
+  if (!feed?.length) {
+    return null
+  }
+
+  return (
+    <div>
+      <div className='flex justify-center my-10'>
+        <UserCard user={feed[0]} />
+      </div>
+    </div>
+  )
+}
+
+export default Feed
