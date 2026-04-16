ce3bb065149a3024ea0229ee6463d8e9275dfdb2
chandra parmar <chandra.parmar2004@gmail.com>
2026-04-17 00:32:17 +0530

    fix usercard

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -1,11 +1,28 @@
-
+import { useDispatch } from "react-redux"
+import { removeUserFromFeed } from "../utils/slices/feedSlice"
+import axios from 'axios'
 
 function Card({ user }) {
-    if (!user) {
-        return null
-    }
+    
 
-    const { firstName, lastName, age, photoUrl, gender, about } = user
+    const {_id , firstName, lastName, age, photoUrl, gender, about } = user
+    const dispatch = useDispatch()
+
+    const handleSendRequest = async (status, userId) =>{
+        try{
+          
+            const res = axios.post(`http://localhost:5001/api/request/send/${status}/${userId}`,{},{
+                withCredentials:true
+            })
+
+            //remove the card from the feed
+            dispatch(removeUserFromFeed(userId))
+
+        }catch(err)
+        {
+          setError(err?.response?.data?.message || "Failed to fetch connections. Please try again.")
+        }
+    }
 
     return(
         
@@ -27,8 +44,8 @@ function Card({ user }) {
 
                     <div className="card-actions justify-center my-4">
 
-                       <button className="btn btn-primary">Ignore</button>
-                       <button className="btn btn-primary">Interested</button>
+                       <button className="btn btn-primary" onClick={()=> handleSendRequest("ignored",_id)}>Ignore</button>
+                       <button className="btn btn-primary" onClick={()=> handleSendRequest('interested',_id)}>Interested</button>
 
                     </div>
                 </div>
