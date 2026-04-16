1aa305cd5bef8914b50f2444ac725a0056354a9a
chandra parmar <chandra.parmar2004@gmail.com>
2026-04-17 00:40:58 +0530

    route added for connection, requests,and chat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Body from './Body'
 import Login from './components/Login'
+import { Toaster } from 'react-hot-toast'
 
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
@@ -9,6 +10,9 @@ import { Provider } from 'react-redux';
 import appStore from './utils/appStore';
 import Feed from './components/Feed';
 import Profile from './components/Profile';
+import Connections from './components/Connections'
+import Requests from './components/Requests';
+import Chat from './components/Chat';
 
 
 function App() {
@@ -18,13 +22,19 @@ function App() {
         <BrowserRouter basename='/'>
           <Routes>
             <Route path='/' element={<Body />}> 
-              <Route index element={<Feed />} />
-              <Route path='feed' element={<Feed />} />
+
+              {/* <Route index element={<Feed />} /> */}
+              <Route path='/feed' element={<Feed />} />
+               <Route path='/login' element={<Login />} />
               <Route path='profile' element={<Profile />} />
+              <Route path='/connections' element={ <Connections></Connections>}></Route>
+               <Route path='/requests' element={<Requests></Requests>}></Route>
+               <Route path='/chat/:targetUserId' element={<Chat></Chat>}></Route>
             </Route>
-            <Route path='login' element={<Login />} />
+           
           </Routes>
         </BrowserRouter>
+        <Toaster position='top-center' />
       </Provider>
     </div>
   )
